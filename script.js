0d4bfb4906a4f3e416243ec181c59bcdba437561
kimbaesung808 <kimbaesung808@gmail.com>
2025-05-15 16:30:01 -1000

    Add user input to task list
    A checkbox was added to the left of the task for checking/unchecking
    tasks and a delete button was added to the right of the task for
    deleting a task.

--- script.js
+++ script.js
@@ -4,4 +4,21 @@ const submitBtn = document.querySelector("#submit-btn");
 submitBtn.addEventListener("click", () => {
   const inputText = text.value;
   text.value = "";
+
+  const list = document.querySelector("ul");
+  const listItem = document.createElement("li");
+
+  const checkbox = document.createElement("input");
+  checkbox.setAttribute("type", "checkbox");
+  listItem.appendChild(checkbox);
+
+  const span = document.createElement("span");
+  span.textContent = inputText;
+  listItem.appendChild(span);
+
+  const deleteBtn = document.createElement("button");
+  deleteBtn.textContent = "Delete";
+  listItem.appendChild(deleteBtn);
+  
+  list.appendChild(listItem);
 });
