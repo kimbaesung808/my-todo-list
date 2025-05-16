d4aded188e80a8319204bd0b1b145cf92a47fa72
kimbaesung808 <kimbaesung808@gmail.com>
2025-05-15 14:46:23 -1000

    Get user input when submit button is clicked

---
+++ script.js
@@ -0,0 +1,6 @@
+const text = document.querySelector("#text-box");
+const submitBtn = document.querySelector("#submit-btn");
+
+submitBtn.addEventListener("click", () => {
+  const inputText = text.value;
+});
